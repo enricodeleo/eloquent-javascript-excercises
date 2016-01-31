aa0e5c66b507d5a1b4394ee76b1a5e8c33725a1a
Enrico Deleo <enricodeleo@Enricos-MacBook-Pro.local>
2016-01-31 23:06:51 +0100

    second part of the excercise

--- chapters/3/3.js
+++ chapters/3/3.js
@@ -1,13 +1,12 @@
 //
 // Excercise #3
-// pag. 59
+// pag. 60
 //
 // Write a function countBs that takes a string as its only argument and returns a number that
 // indicates how many uppercase “B” characters are in the string.
 //
 // Next, write a function called countChar that behaves like countBs, except it takes a second argument
 // that indicates the character that is to be counted (rather than counting only uppercase “B” characters).
-// Rewrite countBs to make use of this new function.
 //
 
 'use strict';
@@ -28,8 +27,26 @@ function countBs(string) {
   return _counter;
 }
 
+/**
+ * Returns how many uppercase characters of a kind are in a string
+ * @params {string} the string to test.
+ * @params {string} the character to test.
+ */
+function countChar(string, character) {
+
+  var _counter = 0;
+
+  for (var i = string.length - 1; i >= 0; i--) {
+    if ( string.charAt(i) === character ) {
+      _counter++;
+    }
+  };
+
+  return _counter;
+}
+
 console.log('');
 console.log('Excercise #3');
 console.log('');
 
-console.log(countBs('This is the Best string I could think about. Better than nothing, uh?'));
+console.log(countChar('This is the Best string I could think about. Better than nothing, uh?', 'B'));
