b28a51151bb9d68b135bb3b4313c9c388aa2ab2f
Enrico Deleo <enricodeleo@Enricos-MacBook-Pro.local>
2016-01-31 23:01:31 +0100

    basic files

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 // ------------------------
 // Excercises from the book
 //
+
+console.log('This is just a placeholder');
+
+//
+// TODO:
+// check if Nodejs or Browser and ask the user to input the excercise
+// he wants to test or list all of them and let him select.
+//
